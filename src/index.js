1bd59c1769e9af93ebe342b8e3e57d6ca8a6a458
loown101 <loown101@gmail.com>
2021-02-28 18:03:12 +0300

    init: start bracket-task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,62 @@
+const isOpen = (bracket, bracketsConfig) => {
+  for (let j = 0; j < bracketsConfig.length; j++) {
+
+      if (bracket == bracketsConfig[j][0]) {
+        return j;
+      }
+    }
+    return null; 
+}
+
+const isClose = (bracket, bracketsConfig) => {
+  for (let j = 0; j < bracketsConfig.length; j++) {
+
+      if (bracket == bracketsConfig[j][1]) {
+        return j;
+      }
+    }
+    return null;
+}
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arrStak = [];
+
+    for (let i = 0; i < str.length; i++) { 
+      let resultOpen = isOpen(str[i], bracketsConfig);
+      let resultClose = isClose(str[i], bracketsConfig);
+      
+      if (resultOpen == resultClose)
+      {
+      	let top = arrStak[arrStak.length - 1];
+        if (top == resultClose)
+        {
+        	arrStak.pop();
+        }
+        else {
+        	arrStak.push(resultOpen);
+        }
+        
+        continue;
+      } 
+      
+      if (resultOpen !== null) {
+          arrStak.push(resultOpen);
+          continue;
+      }
+
+      
+
+      if (resultClose !== null ) {
+      	let resultPop = arrStak.pop();
+      	if (resultPop !== resultClose) {
+              return false;
+        }
+      }
+    }
+    
+    if (arrStak.length == 0) {
+    	return true
+    } else {
+    	return false
+  }
 }
